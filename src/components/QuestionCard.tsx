6fda5463ee4a3e7a88f3bfaaea34feec6587ecec
Floyd Manuel <floydmanuel011@gmail.com>
2021-10-12 18:26:15 +0530

    initializing css

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from "react";
+import {AnswerObject} from '../App';
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  //   userAnswer: string;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  console.log(answers);
+
+  return (
+    <div className="card">
+      <p className="number">
+        Question: {questionNr}/{totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div >
+
+        {answers.map(answer => 
+         ( <div key={answer}>
+            <button disabled={!!userAnswer} value={answer} onClick={callback}>                       {/* converting to boolean {!!userAnswer} */}
+            {/* <button disabled={userAnswer ? true :false} value={answer} onClick={callback}> */}   
+                <span dangerouslySetInnerHTML={{__html: answer}}></span>
+            </button>
+          </div>)
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
